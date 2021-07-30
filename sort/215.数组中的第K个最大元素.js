1f25b55c2b7ffdb76054f73da88c01f07e8808b8
Zhaolu Guo <guozhaolu2008@163.com>
2021-07-30 21:38:42 +0800

    排序-增加优化的快速排序

--- sort/215.数组中的第K个最大元素.js
+++ sort/215.数组中的第K个最大元素.js
@@ -9,12 +9,14 @@
  * @param {number} k
  * @return {number}
  */
-// 懒人解法
+// 1.懒人解法
 var findKthLargest1 = function (nums, k) {
     nums.sort((a, b) => b - a);
     return nums[k - 1];
 };
-var findKthLargest = function (nums, k) {
+
+// 2.快速排序（提交报错）
+var findKthLargest2 = function (nums, k) {
     nums.quickSort();
     return nums[k - 1];
 };
@@ -39,5 +41,71 @@ Array.prototype.quickSort = function () {
     const res = rec(this);
     res.forEach((n, i) => { this[i] = n });
 };
+
+// 3.快速搜索
+var findKthLargest3 = function (nums, k) {
+    return nums.quickSelect(k);
+};
+// 快速搜索（由快速排序修改）
+Array.prototype.quickSelect = function (pos) {
+    const rec = function (arr, p) {
+        const len = arr.length;
+        if (len < 2) { return arr[0]; }
+        const mid = arr[0];
+        const right = [];
+        const left = [];
+        for (let i = 1; i < len; i++) {
+            if (arr[i] > mid) {
+                right.push(arr[i]);
+            } else {
+                left.push(arr[i]);
+            }
+        }
+        const rLen = right.length;
+        if (rLen === p - 1) {
+            return mid;
+        } else if (rLen < p) {
+            return rec(left, p - rLen - 1);
+        } else {
+            return rec(right, p);
+        }
+    };
+    return rec(this, pos);
+};
+
+// 4.可以通过提交的快速排序
+var findKthLargest4 = function (nums, k) {
+    nums.quickSort2();
+    return nums[nums.length - k];
+};
+// 优化的快速排序
+Array.prototype.quickSort2 = function (left, right) {
+    if (left === undefined || right === undefined) {
+        left = 0;
+        right = this.length - 1;
+    }
+    if (left >= right) {
+        return;
+    }
+    let temp = this[left];
+    let i = left, j = right;
+    while (i != j) {
+        while (this[j] > temp && i < j) {
+            j--;
+        }
+        while (this[i] <= temp && i < j) {
+            i++;
+        }
+        if (i < j) {
+            [this[i], this[j]] = [this[j], this[i]];
+        }
+    }
+    this[left] = this[i];
+    this[i] = temp;
+    this.quickSort2(left, i - 1);
+    this.quickSort2(i + 1, right);
+};
+
+
 const a = [1, 2, 3, 4, 5, 6, 7, 8];
-console.log(findKthLargest(a, 1));
+console.log(findKthLargest4(a, 1));
--- sort/basic/quickSort.js
+++ sort/basic/quickSort.js
@@ -1,5 +1,5 @@
-// 快速排序
-Array.prototype.quickSort = function () {
+// 快速排序1
+Array.prototype.quickSort1 = function () {
     const rec = (arr) => {
         if (arr.length < 2) { return arr; }
         const left = [];
@@ -17,7 +17,38 @@ Array.prototype.quickSort = function () {
     const res = rec(this);
     res.forEach((n, i) => { this[i] = n });
 };
+// 快速排序2
+Array.prototype.quickSort2 = function (left, right) {
+    if (left === undefined || right === undefined) {
+        left = 0;
+        right = this.length - 1;
+    }
+    if (left >= right) {
+        return;
+    }
+    let temp = this[left];
+    let i = left, j = right;
+    while (i != j) {
+        while (this[j] > temp && i < j) {
+            j--;
+        }
+        while (this[i] <= temp && i < j) {
+            i++;
+        }
+        if (i < j) {
+            [this[i], this[j]] = [this[j], this[i]];
+        }
+    }
+    this[left] = this[i];
+    this[i] = temp;
+    this.quickSort2(left, i - 1);
+    this.quickSort2(i + 1, right);
+};
 
-const arr = [5, 6, 10, 232, 4, 3, 2, 1, 0, -1];
-arr.quickSort();
-console.log(arr);
+const arr1 = [5, 6, 10, 232, 4, 3, 2, 1, 0, -1, -1, -1];
+arr1.quickSort1();
+console.log(arr1);
+
+const arr2 = [5, 6, 10, 232, 4, 3, 2, 1, 0, -1, -1, -1];
+arr2.quickSort2();
+console.log(arr2);
