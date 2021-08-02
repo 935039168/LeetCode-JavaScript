6b054a17f49b990436a37221657ecd4282d5d871
Zhaolu Guo <guozhaolu2008@163.com>
2021-08-02 16:02:55 +0800

    增加堆排序

--- sort/215.数组中的第K个最大元素.js
+++ sort/215.数组中的第K个最大元素.js
@@ -105,7 +105,7 @@ Array.prototype.quickSort4 = function (left, right) {
     this.quickSort4(left, i - 1);
     this.quickSort4(i + 1, right);
 };
-// 5.快速搜索（官方翻译版）
+// 5.快速搜索（官方题解1翻译版）
 var findKthLargest5 = function (nums, k) {
     return quickSelect5(nums, 0, nums.length - 1, nums.length - k);
 };
@@ -118,7 +118,7 @@ const quickSelect5 = function (a, l, r, index) {
     }
 };
 const randomPartition5 = function (a, l, r) {
-    const i = parseInt(Math.random() * (r - l + 1) + l);
+    const i = parseInt(Math.random() * (r - l + 1) + l);// 随机一个l与r之间的整数
     swap5(a, i, r);
     return partition5(a, l, r);
 };
@@ -137,7 +137,54 @@ const swap5 = function (a, i, j) {
     a[i] = a[j];
     a[j] = temp;
 };
+// 6.堆排序
+// 第一步构建初始堆：是自底向上构建，从最后一个非叶子节点开始。
+// 第二步就是下沉操作让尾部元素与堆顶元素交换，最大值被放在数组末尾，并且缩小数组的length，不参与后面大顶堆的调整
+// 第三步就是调整：是从上到下，从左到右,因为堆顶元素下沉到末尾了，要重新调整这颗大顶堆
+// 来源：https://leetcode-cn.com/problems/kth-largest-element-in-an-array/solution/xie-gei-qian-duan-tong-xue-de-ti-jie-yi-kt5p2/
 
+// 整个流程就是上浮下沉
+var findKthLargest6 = function (nums, k) {
+    let heapSize = nums.length;
+    buildMaxHeap(nums, heapSize); // 构建好了一个大顶堆
+    // 进行下沉 大顶堆是最大元素下沉到末尾
+    for (let i = nums.length - 1; i >= nums.length - k + 1; i--) {
+        swap(nums, 0, i);
+        --heapSize; // 下沉后的元素不参与到大顶堆的调整
+        // 重新调整大顶堆
+        maxHeapify(nums, 0, heapSize);
+    }
+    return nums[0];
+    // 自下而上构建一颗大顶堆
+    function buildMaxHeap(nums, heapSize) {
+        // 从最后一个非叶子节点开始
+        for (let i = Math.floor(heapSize / 2) - 1; i >= 0; i--) {
+            maxHeapify(nums, i, heapSize);
+        }
+    };
+    // 从左向右，自上而下的调整节点
+    function maxHeapify(nums, i, heapSize) {
+        let l = i * 2 + 1;
+        let r = i * 2 + 2;
+        let largest = i;
+        if (l < heapSize && nums[l] > nums[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && nums[r] > nums[largest]) {
+            largest = r;
+        }
+        if (largest !== i) {
+            swap(nums, i, largest); // 进行节点调整
+            // 继续调整下面的非叶子节点
+            maxHeapify(nums, largest, heapSize);
+        }
+    };
+    function swap(a, i, j) {
+        let temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+};
 
-const a = [1, 2, 3, 4, 5, 6, 7, 8];
-console.log(findKthLargest5(a, 1));
+const a = [3, 2, 3, 1, 2, 4, 5, 5, 6];
+console.log(findKthLargest6(a, 4));
