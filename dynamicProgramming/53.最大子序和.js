21ca3ac5b813f19180d2c0fc4bbed3bca087fe6d
Zhaolu Guo <guozhaolu2008@163.com>
2021-08-18 17:28:14 +0800

    增加注释

--- dynamicProgramming/53.最大子序和.js
+++ dynamicProgramming/53.最大子序和.js
@@ -2,13 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {
+var maxSubArray1 = function (nums) {
     let p = 0;// 当前位置的最大子序和
     let r = nums[0];// 当前最大值
     nums.forEach((x) => {
         p = Math.max(x, p + x);
         r = Math.max(r, p);
     });
+    return r;
+};
+
+var maxSubArray = function (nums) {
+    let p = 0;// 当前位置的最大子序和
+    let r = nums[0];// 当前最大值
+    const len = nums.length;
+    for (let i = 0; i < len; i++) {
+        p = Math.max(nums[i], nums[i] + p);// 比较 当前元素 与 当前元素+前值，如果小于当前元素，则重新从当前元素开始
+        if (p > r) { r = p; }
+    }
     return r;
 };
 
