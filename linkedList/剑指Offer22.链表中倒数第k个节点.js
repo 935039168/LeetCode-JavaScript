969052e216d85f23dcd38516666dfaea0d9c84c0
Zhaolu Guo <guozhaolu2008@163.com>
2021-09-02 09:30:55 +0800

    快慢指针-剑指Offer25.合并两个排序的链表

---
+++ linkedList/剑指Offer22.链表中倒数第k个节点.js
@@ -0,0 +1,24 @@
+// https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var getKthFromEnd = function (head, k) {
+    let p1 = p2 = head;
+    for (let i = 0; i < k; i++) {
+        p2 = p2.next;
+    }
+    while (p2 != null) {
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+    return p1;
+};
