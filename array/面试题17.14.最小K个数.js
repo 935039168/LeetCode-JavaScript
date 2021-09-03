15a45b091601c7ec83a8dfd87c9b9f2d096d2a55
Zhaolu Guo <guozhaolu2008@163.com>
2021-09-03 17:26:48 +0800

    重新造一遍轮子

--- array/面试题17.14.最小K个数.js
+++ array/面试题17.14.最小K个数.js
@@ -10,7 +10,7 @@ var smallestK1 = function (arr, k) {
 };
 // https://leetcode-cn.com/problems/smallest-k-lcci/solution/zui-xiao-kge-shu-by-leetcode-solution-o5eg/
 // 快排
-var smallestK = function (arr, k) {
+var smallestK2 = function (arr, k) {
     const randomizedSelected = (l, r, k) => {
         if (l >= r) {
             return;
@@ -41,7 +41,7 @@ var smallestK = function (arr, k) {
             }
         }
         swap(i + 1, r);
-        return i + 1;
+        return i + 1;// 返回排序划分点
     };
     const swap = (i, j) => {
         [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -50,3 +50,42 @@ var smallestK = function (arr, k) {
     randomizedSelected(0, arr.length - 1, k);
     return arr.slice(0, k);
 };
+// 重新造一次轮子
+var smallestK = function (arr, k) {
+    function sort(l, r, k) {
+        if (l >= r) { return; }
+        const pos = doSort(l, r);// 标记位置
+        const num = pos - l + 1;// 标记本次操作后，划分到flag（含）之前的数量
+        if (num === k) {
+            return;
+        } else if (num > k) {
+            sort(l, pos - 1, k);
+        } else {
+            sort(pos + 1, r, k - num);
+        }
+    }
+    function doSort(l, r) {
+        getRandom(l, r);
+        const flag = arr[r];
+        let i = l - 1;
+        for (let j = l; j < r; j++) {
+            if (arr[j] <= flag) {// 判断条件<=
+                i++;
+                swap(i, j);
+            }
+        }
+        swap(i + 1, r);
+        return i + 1;
+    }
+    function getRandom(l, r) {
+        const num = parseInt(Math.random() * (r - l)) + l;
+        swap(r, num);
+    }
+    function swap(x, y) {
+        [arr[x], arr[y]] = [arr[y], arr[x]];
+    }
+    sort(0, arr.length - 1, k);
+    return arr.slice(0, k);
+};
+
+console.log(smallestK([1, 3, 5, 7, 2, 4, 6, 8], 4)); // [1,2,3,4]
