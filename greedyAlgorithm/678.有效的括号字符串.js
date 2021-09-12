6f7a4eee35d1986dd81c78a65a9db61483a06d53
935039168 <935039168@qq.com>
2021-09-12 22:43:15 +0800

    贪心-有效的括号字符串

---
+++ greedyAlgorithm/678.有效的括号字符串.js
@@ -0,0 +1,25 @@
+// https://leetcode-cn.com/problems/valid-parenthesis-string/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function (s) {
+    let min = max = 0, len = s.length;
+    for (let i = 0; i < len; i++) {
+        const t = s[i];
+        if (t === "(") {
+            min++;
+            max++;
+        } else if (t === ")") {
+            min = Math.max(min - 1, 0);
+            max--;
+            if (max < 0) {
+                return false;
+            }
+        } else {
+            min = Math.max(min - 1, 0);
+            max++;
+        }
+    }
+    return min === 0;
+};
