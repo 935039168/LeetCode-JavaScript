7bf058d3b5aa5f991ab6c014bfd570ce15aaa95b
Zhaolu Guo <guozhaolu2008@163.com>
2021-10-07 23:05:12 +0800

    增加题解

--- array/414.第三大的数.js
+++ array/414.第三大的数.js
@@ -9,11 +9,28 @@
  * @param {number[]} nums
  * @return {number}
  */
-var thirdMax = function (nums) {
+var thirdMax1 = function (nums) {
     nums = Array.from(new Set(nums));
     nums.sort((a, b) => b - a);
     if (nums.length >= 3) { return nums[2]; }
     return nums[0];
+};
+// 题解： https://leetcode-cn.com/problems/third-maximum-number/solution/di-san-da-de-shu-by-leetcode-solution-h3sp/
+var thirdMax = function (nums) {
+    let a = b = c = -Number.MAX_VALUE;
+    for (const num of nums) {
+        if (num > a) {
+            c = b;
+            b = a;
+            a = num;
+        } else if (a > num && num > b) {
+            c = b;
+            b = num;
+        } else if (b > num && num > c) {
+            c = num;
+        }
+    }
+    return c === -Number.MAX_VALUE ? a : c;
 };
 // @lc code=end
 
