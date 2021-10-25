f3b0aa69ae5af05a1be2a7c0587b7190b594e179
Zhaolu Guo <guozhaolu2008@163.com>
2021-10-25 20:07:01 +0800

    二叉搜索树-搜索二维矩阵

---
+++ tree/240.搜索二维矩阵-ii.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=javascript
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+// 链接：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/solution/sou-suo-er-wei-ju-zhen-ii-by-leetcode-so-9hcx/
+var searchMatrix1 = function (matrix, target) {
+    const m = matrix.length, n = matrix[0].length;
+    let x = 0, y = n - 1;
+    while (x < m && y >= 0) {
+        if (matrix[x][y] === target) {
+            return true;
+        }
+        if (matrix[x][y] > target) {
+            --y;
+        } else {
+            ++x;
+        }
+    }
+    return false;
+};
+
+var searchMatrix = function (matrix, target) {
+    for (const row of matrix) {
+        const index = search(row, target);
+        if (index >= 0) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const search = (nums, target) => {
+    let low = 0, high = nums.length - 1;
+    while (low <= high) {
+        const mid = Math.floor((high - low) / 2) + low;
+        const num = nums[mid];
+        if (num === target) {
+            return mid;
+        } else if (num > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+};
+// @lc code=end
