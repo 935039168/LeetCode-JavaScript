58e7295d27dd399689948d81dda40bca41c8c93c
Zhaolu Guo <guozhaolu2008@163.com>
2021-11-04 20:34:51 +0800

    数学-有效的完全平方数

---
+++ math/367.有效的完全平方数.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=367 lang=javascript
+ *
+ * [367] 有效的完全平方数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+    const x = Math.floor(Math.sqrt(num));
+    return x * x === num;
+};
+// @lc code=end
+
