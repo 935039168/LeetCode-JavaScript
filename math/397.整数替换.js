6e0e144ebf432403e555da6d808d3de2ae184de7
Zhaolu Guo <guozhaolu2008@163.com>
2021-11-19 09:29:00 +0800

    增加题解

--- math/397.整数替换.js
+++ math/397.整数替换.js
@@ -5,7 +5,9 @@
  */
 // 递归
 var integerReplacement1 = function (n) {
-    if (n === 1) { return 0; }
+    if (n === 1) {
+        return 0;
+    }
     let res = 1;
     if (n % 2 === 1) {
         res += Math.min(integerReplacement(n - 1), integerReplacement(n + 1));
@@ -14,20 +16,34 @@ var integerReplacement1 = function (n) {
     }
     return res;
 };
+var integerReplacement2 = function (n) {
+    let res = 0;
+    while (n !== 1) {
+        if (n === 3) {
+            n = n - 1;
+        } else if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = (n + 1) % 4 === 0 ? n + 1 : n - 1;
+        }
+        res++;
+    }
+    return res;
+};
 // https://leetcode-cn.com/problems/integer-replacement/solution/wu-fu-hao-you-yi-qi-shu-chu-li-by-zw-l-jm1o/
 var integerReplacement = function (n) {
     let res = 0;
     while (n > 1) {
-        if (n === 3) {// 对3特殊处理
+        if (n === 3) { // 对3特殊处理
             n = n - 1;
-        } else if (n % 2 === 1) {// 能被4整除,可以保证基数操作尽可能少
+        } else if (n % 2 === 1) { // 能被4整除,可以保证基数操作尽可能少
             n = (n + 1) % 4 === 0 ? n + 1 : n - 1;
         } else {
-            n = n >>> 1;// 无符号右移
+            n = n >>> 1; // 无符号右移！
         }
         res++;
     }
     return res;
 };
 
-console.log(integerReplacement(999999999));// 39
+console.log(integerReplacement(999999999)); // 39
