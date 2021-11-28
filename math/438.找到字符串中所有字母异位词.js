9dfb4651258bd363ea6a163533253e5ebbabab66
Zhaolu Guo <guozhaolu2008@163.com>
2021-11-28 23:56:11 +0800

    增加题解

--- math/438.找到字符串中所有字母异位词.js
+++ math/438.找到字符串中所有字母异位词.js
@@ -17,6 +17,7 @@ var findAnagrams1 = function (s, p) {
         t = sLen - pLen,
         map = new Map(),
         res = [];
+    if (sLen < pLen) return res;
     for (let i = 0; i < pLen; i++) {
         map.get(p[i]) === undefined ?
             map.set(p[i], 1) :
@@ -39,6 +40,82 @@ var findAnagrams1 = function (s, p) {
         }
     }
     return res;
+};
+// https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/solution/zhao-dao-zi-fu-chuan-zhong-suo-you-zi-mu-xzin/
+// 滑动串口
+var findAnagrams2 = function (s, p) {
+    const sLen = s.length,
+        pLen = p.length;
+    if (sLen < pLen) return [];
+    const ans = [];
+    const sCount = new Array(26).fill(0);
+    const pCount = new Array(26).fill(0);
+    const acca = 'a'.charCodeAt();
+    for (let i = 0; i < pLen; ++i) {
+        ++sCount[s[i].charCodeAt() - acca];
+        ++pCount[p[i].charCodeAt() - acca];
+    }
+    if (sCount.toString() === pCount.toString()) {
+        ans.push(0);
+    }
+    for (let i = 0; i < sLen - pLen; ++i) {
+        --sCount[s[i].charCodeAt() - acca];
+        ++sCount[s[i + pLen].charCodeAt() - acca];
+        if (sCount.toString() === pCount.toString()) {
+            ans.push(i + 1);
+        }
+    }
+    return ans;
+};
+// 改良的滑动串口
+var findAnagrams = function (s, p) {
+    const sLen = s.length,
+        pLen = p.length;
+    if (sLen < pLen) return [];
+    const ans = [];
+    const sCount = new Array(26).fill(0);
+    const pCount = new Array(26).fill(0);
+    const acca = 'a'.charCodeAt();
+    for (let i = 0; i < pLen; ++i) {
+        ++sCount[s[i].charCodeAt() - acca];
+        ++pCount[p[i].charCodeAt() - acca];
+    }
+    if (sCount.toString() === pCount.toString()) {
+        ans.push(0);
+    }
+    for (let i = 0; i < sLen - pLen; ++i) {
+        --sCount[s[i].charCodeAt() - acca];
+        ++sCount[s[i + pLen].charCodeAt() - acca];
+        if (sCount.toString() === pCount.toString()) {
+            ans.push(i + 1);
+        }
+    }
+    return ans;
+};
+// 非常快的方法
+var findAnagrams = function (s, p) {
+    let ret = [];
+    let arr = new Array(26).fill(0);
+    let asciiA = "a".charCodeAt(0);
+    for (let i = 0; i < p.length; i++) {
+        let curCode = p.charCodeAt(i);
+        arr[curCode - asciiA]++;
+    }
+    let arrS = new Array(26).fill(0);
+    let left = 0;
+    for (let i = 0; i < s.length; i++) {
+        let code = s.charCodeAt(i) - asciiA;
+        arrS[code]++;
+        while (arrS[code] > arr[code]) {
+            let leftCode = s.charCodeAt(left) - asciiA;
+            arrS[leftCode]--;
+            left++;
+        }
+        if (i - left == p.length - 1) {
+            ret.push(left);
+        }
+    }
+    return ret;
 };
 console.log(findAnagrams("abcabccbbaa", "aabbcc")); // [0,5]
 console.log(findAnagrams("cbaebabacd", "abc")); // [0,6]
