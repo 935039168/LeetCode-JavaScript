315878854aa7e406b4a14074033a5a64c9b2af86
Zhaolu Guo <guozhaolu2008@163.com>
2021-11-30 23:20:06 +0800

    增加二分法

--- math/400.第-n-位数字.js
+++ math/400.第-n-位数字.js
@@ -45,8 +45,35 @@ var findNthDigit2 = function (n) {
     return digit;
 };
 // 二分法
-var findNthDigit = function (n) {
-    
+var findNthDigit = function(n) {
+    const totalDigits = (length) => {
+        let digits = 0;
+        let curLength = 1, curCount = 9;
+        while (curLength <= length) {
+            digits += curLength * curCount;
+            curLength++;
+            curCount *= 10;
+        }
+        return digits;
+    };
+
+    let low = 1, high = 9;
+    while (low < high) {
+        const mid = Math.floor((high - low) / 2) + low;
+        if (totalDigits(mid) < n) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    const d = low;
+    const prevDigits = totalDigits(d - 1);
+    const index = n - prevDigits - 1;
+    const start = Math.floor(Math.pow(10, d - 1));
+    const num = start + Math.floor(index / d);
+    const digitIndex = index % d;
+    const digit = Math.floor(num / Math.floor(Math.pow(10, d - digitIndex - 1))) % 10;
+    return digit;
 };
 console.log(findNthDigit(2147483647)); // 2
 console.log(findNthDigit(874)); // 3
