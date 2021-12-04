d9d743f8480464b1458fabd5b1cab2cb59619fde
Zhaolu Guo <guozhaolu2008@163.com>
2021-12-04 17:58:02 +0800

    Create 383.赎金信.js
    连续简单题目

---
+++ string/383.赎金信.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const arr = new Array(26).fill(0),
+        v = "a".charCodeAt(0),
+        l1 = magazine.length,
+        l2 = ransomNote.length;
+    let ans = true;
+    for(let i=0;i<l1;i++){
+        arr[magazine[i].charCodeAt(0)-v]++;
+    }
+    for(let i=0;i<l2;i++){
+        --arr[ransomNote[i].charCodeAt(0)-v]<0?ans=false:ans;
+    }
+    return ans;
+};
