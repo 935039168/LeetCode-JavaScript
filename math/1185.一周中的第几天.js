0c304d924500e49a56bb66e382403f96c241b92a
935039168 <935039168@qq.com>
2022-01-03 22:13:11 +0800

    API大法好

--- math/1185.一周中的第几天.js
+++ math/1185.一周中的第几天.js
@@ -11,7 +11,7 @@
  * @param {number} year
  * @return {string}
  */
-var dayOfTheWeek = function(day, month, year) {
+var dayOfTheWeek1 = function(day, month, year) {
     const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
         monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
     let days = 365 * (year - 1971) + Math.floor((year - 1969) / 4);
@@ -23,5 +23,11 @@ var dayOfTheWeek = function(day, month, year) {
     }
     days += day;
     return week[(days + 3) % 7];
+};
+// API大发好
+var dayOfTheWeek = function(day, month, year) {
+    let weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let now = new Date(year, month - 1, day);
+    return weeks[now.getDay()];
 };
 // @lc code=end
