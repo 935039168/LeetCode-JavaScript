d67b11c6e16d2c4674f62e0a934535a14b7d5639
Zhaolu Guo <guozhaolu2008@163.com>
2022-02-09 14:50:01 +0800

    增加哈希解法

--- array/2006.差的绝对值为 K 的数对数目.js
+++ array/2006.差的绝对值为 K 的数对数目.js
@@ -4,13 +4,24 @@
  * @return {number}
  */
 // 暴力
-var countKDifference = function (nums, k) {
+var countKDifference1 = function (nums, k) {
     let ans = 0;
     const n = nums.length;
     for (let i = 0; i < n - 1; i++) {
         for (let j = i + 1; j < n; j++) {
             if (Math.abs(nums[i] - nums[j]) === k) ans++;
         }
+    }
+    return ans;
+};
+// 哈希
+var countKDifference = function (nums, k) {
+    let ans = 0;
+    const n = nums.length,
+        map = new Map();
+    for (let i = 0; i < n; i++) {
+        ans += (map.get(nums[i] - k) || 0) + (map.get(nums[i] + k) || 0);
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
     }
     return ans;
 };
