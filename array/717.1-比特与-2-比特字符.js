d4a369410adb519a4a714bd16ea7a0d1d5bbc7e5
Zhaolu Guo <guozhaolu2008@163.com>
2022-02-20 23:31:58 +0800

    正序遍历（字符编码的含义唯一！）

--- array/717.1-比特与-2-比特字符.js
+++ array/717.1-比特与-2-比特字符.js
@@ -9,13 +9,24 @@
  * @param {number[]} bits
  * @return {boolean}
  */
-var isOneBitCharacter = function (bits) {
+// 倒序遍历
+var isOneBitCharacter1 = function (bits) {
     const n = bits.length;
     let i = n - 2;
     while (i >= 0 && bits[i]) {
         i--;
     }
     return (n - i) % 2 === 0;
+};
+// 正序遍历（字符编码的含义唯一！）
+var isOneBitCharacter = function (bits) {
+    const n = bits.length;
+    let res = true;
+    for (let i = 0; i < n;) {
+        if (bits[i] === 0) { i++; res = true; }
+        else { i += 2; res = false; }
+    }
+    return res;
 };
 // @lc code=end
 
