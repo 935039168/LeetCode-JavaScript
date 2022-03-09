50411fc01e66da3b0603464d03174674ea9d2b87
Zhaolu Guo <guozhaolu2008@163.com>
2022-03-09 09:42:11 +0800

    默一遍

--- math/2055.蜡烛之间的盘子.js
+++ math/2055.蜡烛之间的盘子.js
@@ -32,9 +32,44 @@ var platesBetweenCandles = function (s, queries) {
     return ans;
 };
 
-// console.log(platesBetweenCandles("**|**|***|", [[2, 5], [5, 9]]));// [2,3]
-// console.log(platesBetweenCandles("***|**|*****|**||**|*", [[1, 17], [4, 5], [14, 17], [5, 11], [15, 16]]));// [9,0,0,0,0]
-console.log(platesBetweenCandles(
-    "*|*||||**|||||||*||*||*||**|*|*||*",
-    [[2, 33], [2, 32], [3, 31], [0, 33], [1, 24], [3, 20], [7, 11], [5, 32], [2, 31], [5, 31], [0, 31], [3, 28], [4, 33], [6, 29], [2, 30], [2, 28], [1, 30], [1, 33], [4, 32], [5, 30], [4, 23], [0, 30], [3, 10], [5, 28], [0, 28], [4, 28], [3, 33], [0, 27]]));
-    // [9,9,9,10,6,4,0,9,9,9,10,7,9,8,8,7,9,10,9,8,5,9,2,7,8,7,9,8]
+console.log(platesBetweenCandles("**|**|***|", [[2, 5], [5, 9]]));// [2,3]
+console.log(platesBetweenCandles("***|**|*****|**||**|*", [[1, 17], [4, 5], [14, 17], [5, 11], [15, 16]]));// [9,0,0,0,0]
+console.log(platesBetweenCandles("*|*||||**|||||||*||*||*||**|*|*||*", [[2, 33], [2, 32], [3, 31], [0, 33], [1, 24], [3, 20], [7, 11], [5, 32], [2, 31], [5, 31], [0, 31], [3, 28], [4, 33], [6, 29], [2, 30], [2, 28], [1, 30], [1, 33], [4, 32], [5, 30], [4, 23], [0, 30], [3, 10], [5, 28], [0, 28], [4, 28], [3, 33], [0, 27]]));// [9,9,9,10,6,4,0,9,9,9,10,7,9,8,8,7,9,10,9,8,5,9,2,7,8,7,9,8]
+
+// 默一遍
+var platesBetweenCandles1 = function (s, queries) {
+    const n = s.length, m = queries.length, preSum = new Array(n).fill(0);
+    let calc = 0;
+    for (let i = 0; i < n; i++) {
+        if (s[i] === '*') {
+            calc++;
+        }
+        preSum[i] = calc;
+    }
+    const left = new Array(n).fill(-1), right = new Array(n).fill(-1);
+    for (let i = 0; i < n; i++) {
+        if (s[i] === '|') {
+            left[i] = i;
+        } else {
+            left[i] = i > 0 ? left[i - 1] : -1;
+        }
+    }
+    for (let i = n - 1; i >= 0; i--) {
+        if (s[i] === '|') {
+            right[i] = i;
+        } else {
+            right[i] = i < n - 1 ? right[i + 1] : -1;
+        }
+    }
+    const res = new Array(m).fill(0);
+    for (let i = 0; i < m; i++) {
+        const x = right[queries[i][0]],
+            y = left[queries[i][1]];
+        if (x !== -1 && y !== -1 && x < y)
+            res[i] = preSum[y] - preSum[x];
+    }
+    return res;
+};
+
+console.log(platesBetweenCandles1("**|**|***|", [[2, 5], [5, 9]]));// [2,3]
+console.log(platesBetweenCandles1("***|**|*****|**||**|*", [[1, 17], [4, 5], [14, 17], [5, 11], [15, 16]]));// [9,0,0,0,0]
