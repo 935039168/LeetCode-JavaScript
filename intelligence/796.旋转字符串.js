9340ff1bece23f1a20ed11c4eb741a8629f16e58
Zhaolu Guo <guozhaolu2008@163.com>
2022-04-07 09:23:44 +0800

    智力题

---
+++ intelligence/796.旋转字符串.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=796 lang=javascript
+ *
+ * [796] 旋转字符串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+    return s.length === goal.length && (s + s).indexOf(goal) > -1;
+};
+// @lc code=end
+
