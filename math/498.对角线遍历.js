2c742e98f6d821c831e7dd68b17d285825cefe8b
Zhaolu Guo <guozhaolu2008@163.com>
2022-06-20 22:48:14 +0800

    math

---
+++ math/498.对角线遍历.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=498 lang=javascript
+ *
+ * [498] 对角线遍历
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+// https://leetcode.cn/problems/diagonal-traverse/solution/pythonjavatypescriptgo-by-himymben-d3uq/
+var findDiagonalOrder = function (mat) {
+    const m = mat.length,
+        n = mat[0].length;
+    const res = [];
+    for (let k = 0; k < m + n - 1; k++) {// 一共m+n-1条对角线
+        if ((k & 1) === 1) {
+            for (let x = Math.max(0, k - n + 1); x < Math.min(k + 1, m); x++) {
+                res.push(mat[x][k - x]);
+            }
+        } else {
+            for (let x = Math.min(k, m - 1); x >= Math.max(0, k - n + 1); x--) {
+                res.push(mat[x][k - x]);
+            }
+        }
+    }
+    return res;
+};
+// @lc code=end
+
